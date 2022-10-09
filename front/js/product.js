e77a87598720f726d6da1526957a41f682a7771d
Pierrick Rousselot <rousselot.pierrick@gmail.com>
2022-10-09 22:33:09 +0200

    Ajout v1 localStorage pour le panier

--- front/js/product.js
+++ front/js/product.js
@@ -1,7 +1,6 @@
-
-    // récupération de l'ID de l'adresse html
+// récupération de l'ID de l'adresse html
 let linkID = new URL(location.href).searchParams.get("id");
-    // insertion de l'url dans la varibla de l'appel de l'API
+// insertion de l'url dans la varibla de l'appel de l'API
 const urlProduct = `http://localhost:3000/api/products/${linkID}`;
 
 async function afficherProduit(){
@@ -40,14 +39,65 @@ async function afficherProduit(){
 
             for (const color of colorsList) {
                 let itemOption = new Option(color, color);
-        
                 let selectOptionList = document.querySelector('#colors')
-
                 selectOptionList.add(itemOption, undefined);
-
         }
     }
 }
 
+
+// APPEL DE LA FONCTION POUR AFFICHER LE PRODUIT EN FONCTION DE L'ID
 afficherProduit()
+
+
+
+//// ------- Recupérer le produit pour le sauvegarder dans le storage 
+
+
+
+// function recupererProduit(){
+//     let button = document.querySelector('#addToCart')
+//     button.addEventListener('click',() => {
+//         alert('Article bien ajouté au panier')
+
+
+//     })
+
+// }
+
+async function recupererProduit(){
+    const requete = await fetch(urlProduct, {
+        method : 'GET'
+    });
+
+    if(!requete.ok){
+        alert('un problème est survenu');
+    } else {
+
+        // JSON.stringfy(objet) => transforme un objet en string
+        // JSON.parse(string) => transforme un string en objet JSON
+        const productSelected = {
+            id : linkID,
+            quantite : quantity.value,
+            couleur : colors.value,
+        }
+        // console.log(productSelected['id'])
+        let button = document.querySelector('#addToCart')
+            button.addEventListener('click',(e) => {
+                
+                localStorage.setItem('color-select', colors.value)
+                localStorage.setItem('itemsQuantity', quantity.value)
+                
+                if(localStorage.getItem('color-select') != null && localStorage.getItem('itemsQuantity') != "0") {
+                    alert('Article bien ajouté au panier')
+                } else {
+                    alert("Veuillez sélectionner une quantité et une couleur")
+                }
+                
+
+        })    
+    }
+}
+
+recupererProduit()
 
