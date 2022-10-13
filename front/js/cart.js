022a48b2eae2bcd124597d8bbce9821c16570d99
Pierrick Rousselot <rousselot.pierrick@gmail.com>
2022-10-14 00:08:56 +0200

    Ajout v3 du panier : test d'une nouvelle version d'affichage du panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,5 +1,3 @@
-
-
 // récupération de l'ID de l'adresse html
 // let linkID = new URL(location.href).searchParams.get("id");
 // insertion de l'url dans la varibla de l'appel de l'API
@@ -7,6 +5,7 @@ const urlPanier = `http://localhost:3000/api/products/`;
 
 // récupération du panier et transformation en objet JSON
 let recoverPanier = JSON.parse(localStorage.getItem('panier'));
+
 
 async function displayPanier(){
     const requete = await fetch(urlPanier, {
@@ -18,81 +17,95 @@ async function displayPanier(){
     } else { 
         let productListItem = await requete.json();
 
-for(let i=0; i <= recoverPanier.length; i++){
+        for (const key in recoverPanier) {
+           let IDproduct = recoverPanier[key].id;
+           let colorProduct = recoverPanier[key].couleurs;
+            console.log(IDproduct);
+            console.log(colorProduct);
 
-}
+            let findID = productListItem.find(data => data._id === recoverPanier[key].id);
 
-
-for (const product in productListItem) {
-     let findID = recoverPanier.find(data => data.id === productListItem[product]._id);
-         if (findID != undefined) {
- // selection de la balise section.cart__items
- let selectedSectionCart = document.querySelector('section#cart__items');
- // création de tous les éléments nécessaires à l'affichage de chaque produit du panier
- let createArticleCart = document.createElement('article');
- let createArticleDivCart = document.createElement('div');
- let createArticleDivImgCart = document.createElement('img');
- let createArticleDivCartBis = document.createElement('div');
- let createArticleDivCartBisDescription = document.createElement('div');
- let createDescriptionTitle = document.createElement('h2');
- let createDescriptionColor = document.createElement('p')
- let createDescriptionPrice = document.createElement('p')
- let createArticleDivCartBisSettings = document.createElement('div');
- let createSettingsDivQuantity = document.createElement('div');
- let createSettingQuantity = document.createElement('p');
- let createSettingQuantityInput = document.createElement('input');
- let createSettingsDivDelete = document.createElement('div');
- let createSettingDelete = document.createElement('p');
- // ------------------------------------------------------------------------------
- // création de la partie article après la balise section
- selectedSectionCart.appendChild(createArticleCart).classList.add('cart__item');
- // création de la partie div + img après la balise article
- createArticleCart.appendChild(createArticleDivCart).classList.add('cart__item__img');
- createArticleDivCart.appendChild(createArticleDivImgCart);
- // selection de la balise article
- let getArticleCart = document.querySelector('.cart__item');
- // création de la partie div + description de l'article 
- createArticleCart.appendChild(createArticleDivCartBis).classList.add('cart__item__content');
- createArticleDivCartBis.appendChild(createArticleDivCartBisDescription).classList.add('cart__item__content__description');
- createArticleDivCartBisDescription.appendChild(createDescriptionTitle);
- createArticleDivCartBisDescription.appendChild(createDescriptionColor);
- createArticleDivCartBisDescription.appendChild(createDescriptionPrice);
- // création de la partie div + quantité 
- createArticleDivCartBis.appendChild(createArticleDivCartBisSettings).classList.add('cart__item__content__settings');
- createArticleDivCartBisSettings.appendChild(createSettingsDivQuantity).classList.add('cart__item__content__settings__quantity');
- createSettingsDivQuantity.appendChild(createSettingQuantity);
- createSettingsDivQuantity.appendChild(createSettingQuantityInput).classList.add('itemQuantity');
- createSettingQuantityInput.type = "number";
- createSettingQuantityInput.min = "1";
- createSettingQuantityInput.max = "100";
- createSettingQuantityInput.name = "itemQuantity";
- // création de la partie div + suppression 
- createArticleDivCartBisSettings.appendChild(createSettingsDivDelete).classList.add('cart__item__content__settings__delete');
- createSettingsDivDelete.appendChild(createSettingDelete).classList.add('deleteItem');
-
- // Ajout du contenu dynamique des données du panier selon le produit 
-
- getArticleCart.setAttribute('data-id', `${findID.id}`);
- getArticleCart.setAttribute('data-color', findID.couleurs);
- createArticleDivImgCart.src = productListItem[product].imageUrl;
- createArticleDivImgCart.alt = productListItem[product].imageUrl;
- createDescriptionTitle.textContent = productListItem[product].name;
- createDescriptionPrice.textContent = productListItem[product].description;
- createDescriptionColor.textContent = findID.couleurs;
- createDescriptionPrice.textContent = `${productListItem[product].price} €`;
- createSettingQuantity.textContent = "Quantité :"
- createSettingQuantityInput.value = findID.quantite;
- createSettingDelete.textContent = "Supprimer";
-
-         }
-}
+            console.log(findID);
+            console.log(findID.imageUrl)
+            
+        }
 
     }
 
 }
 
 displayPanier()
-// for (const product in recoverPanier) {
+
+
+
+// ------------------------TEST 1 
+// for (const product in productListItem) {
+
+    //         recoverPanier.forEach(function (produit){
+    //             var ID  = produit.id;
+    //             console.log(ID)
+    //     })
     
-//     console.log(recoverPanier[product]);
-// }
+    //      let findID = recoverPanier.find(data => data.id === productListItem[product]._id);
+    //      console.log(findID)
+    //          if (findID != undefined) {
+    //  // selection de la balise section.cart__items
+    //  let selectedSectionCart = document.querySelector('section#cart__items');
+    //  // création de tous les éléments nécessaires à l'affichage de chaque produit du panier
+    //  let createArticleCart = document.createElement('article');
+    //  let createArticleDivCart = document.createElement('div');
+    //  let createArticleDivImgCart = document.createElement('img');
+    //  let createArticleDivCartBis = document.createElement('div');
+    //  let createArticleDivCartBisDescription = document.createElement('div');
+    //  let createDescriptionTitle = document.createElement('h2');
+    //  let createDescriptionColor = document.createElement('p')
+    //  let createDescriptionPrice = document.createElement('p')
+    //  let createArticleDivCartBisSettings = document.createElement('div');
+    //  let createSettingsDivQuantity = document.createElement('div');
+    //  let createSettingQuantity = document.createElement('p');
+    //  let createSettingQuantityInput = document.createElement('input');
+    //  let createSettingsDivDelete = document.createElement('div');
+    //  let createSettingDelete = document.createElement('p');
+    //  // ------------------------------------------------------------------------------
+    //  // création de la partie article après la balise section
+    //  selectedSectionCart.appendChild(createArticleCart).classList.add('cart__item');
+    //  // création de la partie div + img après la balise article
+    //  createArticleCart.appendChild(createArticleDivCart).classList.add('cart__item__img');
+    //  createArticleDivCart.appendChild(createArticleDivImgCart);
+    //  // selection de la balise article
+    //  let getArticleCart = document.querySelector('.cart__item');
+    //  // création de la partie div + description de l'article 
+    //  createArticleCart.appendChild(createArticleDivCartBis).classList.add('cart__item__content');
+    //  createArticleDivCartBis.appendChild(createArticleDivCartBisDescription).classList.add('cart__item__content__description');
+    //  createArticleDivCartBisDescription.appendChild(createDescriptionTitle);
+    //  createArticleDivCartBisDescription.appendChild(createDescriptionColor);
+    //  createArticleDivCartBisDescription.appendChild(createDescriptionPrice);
+    //  // création de la partie div + quantité 
+    //  createArticleDivCartBis.appendChild(createArticleDivCartBisSettings).classList.add('cart__item__content__settings');
+    //  createArticleDivCartBisSettings.appendChild(createSettingsDivQuantity).classList.add('cart__item__content__settings__quantity');
+    //  createSettingsDivQuantity.appendChild(createSettingQuantity);
+    //  createSettingsDivQuantity.appendChild(createSettingQuantityInput).classList.add('itemQuantity');
+    //  createSettingQuantityInput.type = "number";
+    //  createSettingQuantityInput.min = "1";
+    //  createSettingQuantityInput.max = "100";
+    //  createSettingQuantityInput.name = "itemQuantity";
+    //  // création de la partie div + suppression 
+    //  createArticleDivCartBisSettings.appendChild(createSettingsDivDelete).classList.add('cart__item__content__settings__delete');
+    //  createSettingsDivDelete.appendChild(createSettingDelete).classList.add('deleteItem');
+    
+    //  // Ajout du contenu dynamique des données du panier selon le produit 
+    
+    // //  getArticleCart.setAttribute('data-id', `${findID.id}`);
+    // //  getArticleCart.setAttribute('data-color', findID.couleurs);
+    //  createArticleDivImgCart.src = productListItem[product].imageUrl;
+    //  createArticleDivImgCart.alt = productListItem[product].imageUrl;
+    //  createDescriptionTitle.textContent = productListItem[product].name;
+    //  createDescriptionPrice.textContent = productListItem[product].description;
+    //  createDescriptionColor.textContent = findID.couleurs;
+    //  createDescriptionPrice.textContent = `${productListItem[product].price} €`;
+    //  createSettingQuantity.textContent = "Quantité :"
+    //  createSettingQuantityInput.value = findID.quantite;
+    //  createSettingDelete.textContent = "Supprimer";
+    
+    //          }
+    // }
